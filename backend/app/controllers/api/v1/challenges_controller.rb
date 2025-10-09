f4f383f5f268b6540484c39552c33951ca2a7519
vutrantrongnguyen <quenmatkhaupart3@gmail.com>
2025-10-10 00:20:26 +0900

    Update challenge controller function

--- backend/app/controllers/api/v1/challenges_controller.rb
+++ backend/app/controllers/api/v1/challenges_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ChallengesController < ApplicationController
-        
+        before_action :set_challenge, only: %i[show update destroy]
         # GET api/v1/challenges
         def index
             @challenges = Challenge.all
@@ -10,12 +10,8 @@ module Api
 
         # POST api/v1/challenges
         def create
-            challenge = Challenge.new(
-                title: 'VTTN', 
-                description: 'New description', 
-                start_date: Date.today, 
-                end_date: Date.tomorrow
-            )
+            puts 'Hello world'
+            challenge = Challenge.new(challenges_params)
             if challenge.save
                 render json: { message: 'Challenge added successfully', data: challenge }
             else
@@ -25,14 +21,40 @@ module Api
 
         # GET api/v1/challenges/:id
         def show
+            if @challenge
+                render json: { message: 'Challenge found', data: challenge }
+            else
+                render json: { message: 'Challenge not found', data: challenge.errors }
+            end
         end
 
         # PATCH/PUT api/v1/challenges/:id
         def update
+            puts @challenge
+            puts @challenges_params
+            if @challenge&.update(challenges_params)
+                render json: { message: 'Challenge updated', data: challenge }
+            else
+                render json: { message: 'Challenge not found', data: challenge.errors }
+            end
         end
         
         # DELETE api/v1/challenges/:id
         def destroy
+            if @challenge&.destroy
+                render json: { message: 'Challenge deleted', data: challenge }
+            else
+                render json: { message: 'Challenge not found', data: challenge.errors }
+            end
+        end
+
+        private
+        def set_challenge
+            @challenge = Challenge.find_by(id: params[:id])
+        end
+        
+        def challenges_params
+            params.require(:challenge).permit(:title, :description, :start_date, :end_date)
         end
     end
   end
